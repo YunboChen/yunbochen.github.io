2c23510fc4702639414008ca2981a8c2193e110a
Rainbow <304165203@qq.com>
2021-08-17 18:01:06 +0800

    feat: 添加started-in-5-minutes.md

--- docs/.vuepress/themeConfig.js
+++ docs/.vuepress/themeConfig.js
@@ -22,6 +22,15 @@ module.exports = {
           link:'/tag/vue/',
         }
       ] 
+    },
+    { 
+      text:'学习', 
+      items:[
+        { 
+          text:'TypeScript',
+          link:'/tag/typescript/',
+        },
+      ] 
     }
   ],
 }
